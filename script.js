976c3ff7592bfeb1e320a75e69f7e66faffe925d
kdl222 <kdl222@lehigh.edu>
2023-07-12 21:12:41 -0400

    footer color changing

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // JavaScript code for Mood Ring color mode selection and navigation
-
 // Function to change color mode and navigate to home.html
 function changeColorModeAndNavigate(color) {
   // Set the selected color mode to localStorage
@@ -12,10 +11,12 @@ function changeColorModeAndNavigate(color) {
 // Function to apply color mode to header-menu on home.html
 function applyColorModeToHeader() {
   const headerMenu = document.querySelector('.header-menu');
+  const footerColor = document.querySelector('.footer-color');
   const storedColorMode = localStorage.getItem('colorMode');
   
   if (storedColorMode) {
       headerMenu.classList.add(storedColorMode);
+      footerColor.classList.add(storedColorMode);
   }
 }
 
